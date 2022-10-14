fdc744e5a2ec49d7d7912fc33ce3a2be8c1cd91d
MrMCake <asehr@hotmail.de>
2022-10-14 18:22:37 +0200

    Init

---
+++ utilities/tools/AzureApiCrawler/private/specs/Get-ParametersFromRoot.ps1
@@ -0,0 +1,170 @@
+﻿<#
+.SYNOPSIS
+Extract all parameters from the given API spec parameter root
+
+.DESCRIPTION
+Extract all parameters from the given API spec parameter root (e.g., PUT parameters)
+
+.PARAMETER SpecificationData
+Mandatory. The source content to crawl for data.
+
+.PARAMETER RelevantParamRoot
+Mandatory. The array of root parameters to process (e.g., PUT parameters).
+
+.PARAMETER JSONKeyPath
+Mandatory. The API Path in the JSON specification file to process
+
+.PARAMETER ResourceType
+Mandatory. The Resource Type to investigate
+
+.EXAMPLE
+Get-ParametersFromRoot -SpecificationData @{ paths = @(...); definitions = @{...} } -RelevantParamRoot @(@{ $ref: "../(...)"}) '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.KeyVault/vaults/{vaultName}' -ResourceType 'vaults'
+
+Fetch all parameters (e.g., PUT) from the KeyVault REST path.
+#>
+function Get-ParametersFromRoot {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [hashtable] $SpecificationData,
+
+        [Parameter(Mandatory = $true)]
+        [array] $RelevantParamRoot,
+
+        [Parameter(Mandatory = $true)]
+        [string] $JSONKeyPath,
+
+        [Parameter(Mandatory = $true)]
+        [string] $ResourceType,
+
+        [Parameter(Mandatory = $false)]
+        [int] $Level = 1
+    )
+
+    $definitions = $specificationData.definitions
+    $specParameters = $specificationData.parameters
+
+    $templateData = @()
+
+    $matchingPathObjectParametersRef = ($relevantParamRoot | Where-Object { $_.in -eq 'body' }).schema.'$ref'
+
+    if (-not $matchingPathObjectParametersRef) {
+        # If 'parameters' does not exist (as the API isn't consistent), we try the resource type instead
+        $matchingPathObjectParametersRef = ($relevantParamRoot | Where-Object { $_.name -eq $ResourceType }).schema.'$ref'
+    }
+    if (-not $matchingPathObjectParametersRef) {
+        # If even that doesn't exist (as the API is even more inconsistent), let's try a 'singular' resource type
+        $matchingPathObjectParametersRef = ($relevantParamRoot | Where-Object { $_.name -eq ($ResourceType.Substring(0, $ResourceType.Length - 1)) }).schema.'$ref'
+    }
+
+    $outerParameters = $definitions[(Split-Path $matchingPathObjectParametersRef -Leaf)]
+
+    # Handle resource name
+    # --------------------
+    # Note: The name can be specified in different locations like the PUT statement, but also in the spec's 'parameters' object as a reference
+    # Case: The name in the url is also a parameter of the PUT statement
+    $pathServiceName = (Split-Path $JSONKeyPath -Leaf) -replace '{|}', ''
+    if ($relevantParamRoot.name -contains $pathServiceName) {
+        $param = $relevantParamRoot | Where-Object { $_.name -eq $pathServiceName }
+
+        $parameterObject = @{
+            level       = 0
+            name        = 'name'
+            type        = 'string'
+            description = $param.description
+            required    = $true
+        }
+
+        $parameterObject = Set-OptionalParameter -SourceParameterObject $param -TargetObject $parameterObject
+    } else {
+        # Case: The name is a ref in the spec's 'parameters' object. E.g., { "$ref": "#/parameters/BlobServicesName" }
+        # For this, we need to find the correct ref, as there can be multiple
+        $nonDefaultParameter = $relevantParamRoot.'$ref' | Where-Object { $_ -like '#/parameters/*' } | Where-Object { $specParameters[(Split-Path $_ -Leaf)].name -eq $pathServiceName }
+        if ($nonDefaultParameter) {
+            $param = $specParameters[(Split-Path $nonDefaultParameter -Leaf)]
+
+            $parameterObject = @{
+                level       = 0
+                name        = 'name'
+                type        = 'string'
+                description = $param.description
+                required    = $true
+            }
+
+            $parameterObject = Set-OptionalParameter -SourceParameterObject $param -TargetObject $parameterObject
+        }
+    }
+
+    $templateData += $parameterObject
+
+    # Process outer properties
+    # ------------------------
+    foreach ($outerParameter in $outerParameters.properties.Keys | Where-Object { $_ -ne 'properties' -and -not $outerParameters.properties[$_].readOnly }) {
+        $param = $outerParameters.properties[$outerParameter]
+        $parameterObject = @{
+            level       = 0
+            name        = $outerParameter
+            type        = $param.keys -contains 'type' ? $param.type : 'object'
+            description = $param.description
+            required    = $outerParameters.required -contains $outerParameter
+        }
+
+        $parameterObject = Set-OptionalParameter -SourceParameterObject $param -TargetObject $parameterObject
+
+        $templateData += $parameterObject
+    }
+
+    # Special case: Location
+    # The location parameter is not explicitely documented at this place (even though it should). It is however referenced as 'required' and must be included
+    if ($outerParameters.required -contains 'location') {
+        $parameterObject = @{
+            level       = 0
+            name        = 'location'
+            type        = 'string'
+            description = 'Location for all Resources.'
+            required    = $false
+            default     = ($JSONKeyPath -like '/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/*') ? 'resourceGroup().location' : 'deployment().location'
+        }
+
+        # param location string = resourceGroup().location
+        $templateData += $parameterObject
+    }
+
+    # Process inner properties
+    # ------------------------
+    $innerRef = $outerParameters.properties.properties.'$ref'
+    $innerParameters = $definitions[(Split-Path $innerRef -Leaf)].properties
+
+    foreach ($innerParameter in ($innerParameters.Keys | Where-Object { -not $innerParameters[$_].readOnly })) {
+        $param = $innerParameters[$innerParameter]
+
+        if ($param.Keys -contains '$ref') {
+            switch (($param.'$ref' -split '\/')[1]) {
+                'definitions' {
+                    $recursiveInputObject = @{
+                        SpecificationData = $SpecificationData
+                        RelevantParamRoot = $definitions
+                        JSONKeyPath       = $JSONKeyPath
+                        ResourceType      = $ResourceType
+                    }
+                    $templateData += Get-ParametersFromRoot @recursiveInputObject
+                }
+            }
+        } else {
+            $parameterObject = @{
+                level       = $Level
+                name        = $innerParameter
+                type        = $param.keys -contains 'type' ? $param.type : 'object'
+                description = $param.description
+                required    = $innerParameters.required -contains $innerParameter
+            }
+
+            $parameterObject = Set-OptionalParameter -SourceParameterObject $param -TargetObject $parameterObject
+
+            $templateData += $parameterObject
+        }
+    }
+
+    return $templateData
+}
---
+++ utilities/tools/AzureApiCrawler/private/specs/Get-ServiceSpecPathData.ps1
@@ -0,0 +1,186 @@
+﻿<#
+.SYNOPSIS
+Get the latest API spec file path & service path (in file) for a given ProviderNamespace - ResourceType combination.
+
+.DESCRIPTION
+Get the latest API spec file path & service path (in file) for a given ProviderNamespace - ResourceType combination.
+
+.PARAMETER ProviderNamespace
+Mandatory. The provider namespace to query the data for
+
+.PARAMETER ResourceType
+Mandatory. The resource type to query the data for
+
+.PARAMETER RepositoryPath
+Mandatory. The path of the cloned/downloaded API Specs repository
+
+.PARAMETER IncludePreview
+Optional. Set to also consider 'preview' versions for the request.
+
+.EXAMPLE
+Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.Keyvault' -ResourceType 'vaults' -RepositoryPath './temp/azure-rest-api-specs' -IncludePreview
+
+Get the latest API spec file path & service path for the resource type [Microsoft.KeyVault/vaults] - including the latest preview version (if any)
+#>
+function Get-ServiceSpecPathData {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $ProviderNamespace,
+
+        [Parameter(Mandatory = $true)]
+        [string] $ResourceType,
+
+        [Parameter(Mandatory = $true)]
+        [string] $RepositoryPath,
+
+        [Parameter(Mandatory = $false)]
+        [switch] $IncludePreview
+    )
+
+    try {
+        #find the resource provider folder
+        $resourceProviderFolders = Get-FolderList -rootFolder (Join-Path $RepositoryPath 'specification') -ProviderNamespace $ProviderNamespace
+
+        $resultArr = @()
+        foreach ($resourceProviderFolder in $resourceProviderFolders) {
+            Write-Verbose ('Processing Resource provider folder [{0}]' -f $resourceProviderFolder)
+            # TODO: Get highest API version (preview/non-preview)
+            $apiVersionFoldersArr = @()
+            if (Test-Path -Path $(Join-Path $resourceProviderFolder 'stable')) { $apiVersionFoldersArr += Get-ChildItem -Path $(Join-Path $resourceProviderFolder 'stable') }
+            if ($IncludePreview) {
+                # adding preview API versions if allowed
+                if (Test-Path -Path $(Join-Path $resourceProviderFolder 'preview')) { $apiVersionFoldersArr += Get-ChildItem -Path $(Join-Path $resourceProviderFolder 'preview') }
+            }
+
+            # sorting all API version from the newest to the oldest
+            $apiVersionFoldersArr = $apiVersionFoldersArr | Sort-Object -Property Name -Descending
+            if ($apiVersionFoldersArr.Count -eq 0) {
+                Write-Warning ('No API folder found in folder [{0}]' -f $resourceProviderFolder)
+                continue
+            }
+
+            foreach ($apiversionFolder in $apiVersionFoldersArr) {
+                $putMethods = @()
+                foreach ($jsonFile in $(Get-ChildItem -Path $apiversionFolder -Filter *.json)) {
+                    $jsonPaths = (ConvertFrom-Json (Get-Content -Raw -Path $jsonFile)).paths
+                    $jsonPaths.PSObject.Properties | ForEach-Object {
+                        $put = $_.value.put
+                        # if ($_.Name -contains $ResourceType) {
+                        #     Write-Verbose ('File: [{0}], API: [{1}] JsonKeyPath: [{2}]' -f $jsonFile.Name, $apiversionFolder.Name, $_.Name) -Verbose
+                        # }
+                        if ($put) {
+                            $pathSplit = $_.Name.Split('/')
+                            if (($pathSplit[$pathSplit.Count - 3] -eq $ProviderNamespace) -and ($pathSplit[$pathSplit.Count - 2] -eq $ResourceType)) {
+                                $arrItem = [pscustomobject] @{}
+                                $arrItem | Add-Member -MemberType NoteProperty -Name 'jsonFilePath' -Value $jsonFile.FullName
+                                $arrItem | Add-Member -MemberType NoteProperty -Name 'jsonKeyPath' -Value $_.Name
+                                # $arrItem | Add-Member -MemberType NoteProperty -Name 'putMethod' -Value $_.value.put
+                                $putMethods += $arrItem
+                            }
+                        }
+                    }
+                }
+                if ($putMethods.Count -gt 0) { break } # no scanning of older API folders if a put method already found
+            }
+            $resultArr += $putMethods # adding result of this provider folder to the overall result array
+        }
+    } catch {
+        Write-Error "Error processing [$ProviderNamespace/$ResourceType]: $_"
+        return -2
+    }
+
+    try {
+        Set-Location $initialLocation
+
+        ## Remove temp folder again
+        # $null = Remove-Item $tempFolderPath -Recurse -Force
+
+        if ($resultArr.Count -ge 1) {
+            return $resultArr
+        } else {
+            Write-Error 'No results found'
+            return $resultArr
+        }
+
+    } catch {
+        Write-Error "Error processing [$ProviderNamespace/$ResourceType]: $_"
+        return -2
+    }
+}
+
+# Example call for further processing
+# $result = Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.KeyVault' -ResourceType 'vaults' | Format-List
+# $result | Format-List
+
+# Kris: the below code is for debugging only and will be deleted later.
+## It is commented out and doesn't run, so it can be ignored
+
+# test function calls
+# two examples of working calls for testing
+# Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.KeyVault' -ResourceType 'vaults' | Format-List
+# Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.Storage' -ResourceType 'storageAccounts' | Format-List
+# Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.Compute' -ResourceType 'virtualMachines' | Format-List
+
+# working, multiple results
+# Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.Authorization' -ResourceType 'locks' | Format-List # no results, special case
+# Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.Authorization' -ResourceType 'policyDefinitions' | Format-List
+# Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.Authorization' -ResourceType 'policySetDefinitions' | Format-List
+
+# no results (different ResourceId schema, to be repaired)
+# Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.Resources' -ResourceType 'resourceGroups' | Format-List
+# Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.Security' -ResourceType 'azureSecurityCenter' | Format-List
+
+# working with preview only
+# Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.Authorization' -ResourceType 'policyExemptions' -IncludePreview | Format-List
+# Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.Insights' -ResourceType 'privateLinkScopes' -IncludePreview | Format-List
+# Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.OperationsManagement' -ResourceType 'solutions' -IncludePreview | Format-List
+
+# working. If run without preview, returning one result, with preview: four results
+# Get-ServiceSpecPathData -ProviderNamespace 'Microsoft.Insights' -ResourceType 'diagnosticSettings' -IncludePreview | Format-List
+
+
+
+# running the function against the CARML modules folder
+# to collect some statistics.
+# It requires some modifications of the function Get-ServiceSpecPathData, so please don't use it.
+# below code is temporary and will be deleted later
+# exit # protecting from unnecessary run
+# $carmlModulesRoot = Join-Path $PSScriptRoot '..' '..' '..' 'modules'
+# $resArray = @()
+# foreach ($providerFolder in $(Get-ChildItem -Path $carmlModulesRoot -Filter 'Microsoft.*')) {
+#     foreach ($resourceFolder in $(Get-ChildItem -Path $(Join-Path $carmlModulesRoot $providerFolder.Name) -Directory)) {
+#         Write-Host ('Processing [{0}/{1}]...' -f $providerFolder.Name, $resourceFolder.Name)
+#         $res = Get-ServiceSpecPathData -ProviderNamespace $providerFolder.Name -ResourceType $resourceFolder.Name -IncludePreview
+#         # Get-ServiceSpecPathData -ProviderNamespace $providerFolder.Name -ResourceType $resourceFolder.Name
+
+#         $resArrItem = [pscustomobject] @{}
+#         $resArrItem | Add-Member -MemberType NoteProperty -Name 'Provider' -Value $providerFolder.Name
+#         $resArrItem | Add-Member -MemberType NoteProperty -Name 'ResourceType' -Value $resourceFolder.Name
+#         if ($null -eq $res) {
+#             $resArrItem | Add-Member -MemberType NoteProperty -Name 'Result' -Value 0
+#         } elseif ($res -is [array]) {
+#             $resArrItem | Add-Member -MemberType NoteProperty -Name 'Result' -Value $res.Count
+#         } elseif ($res.GetType().Name -eq 'pscustomobject') {
+#             $resArrItem | Add-Member -MemberType NoteProperty -Name 'Result' -Value 1
+#         } else {
+#             $resArrItem | Add-Member -MemberType NoteProperty -Name 'Result' -Value $res
+#         }
+#         $resArray += $resArrItem
+#     }
+# }
+
+
+
+# # statistics
+
+# $count = $resArray.Count
+# $resArray | ConvertTo-Json -Depth 99
+
+# Write-Host ('Success, more than one result: {0} of {1}' -f ((($resArray | Where-Object { $_.Result -gt 1 }).Count), $count ))
+# Write-Host ('Success, one result: {0} of {1}' -f ((($resArray | Where-Object { $_.Result -eq 1 }).Count), $count))
+# Write-Host ('No Results: {0} of {1}' -f ((($resArray | Where-Object { $_.Result -eq 0 }).Count), $count))
+# Write-Host ('RT Error  : {0} of {1}' -f ((($resArray | Where-Object { $_.Result -eq -2 }).Count), $count))
+
+
